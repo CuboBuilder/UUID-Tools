16b53ab550663b1afbfefcf5ad9570cbf883f1c9
MegaBobus228 <140627917+CuboBuilder@users.noreply.github.com>
2024-09-17 09:10:52 +0300

    Update main.js

--- scripts/main.js
+++ scripts/main.js
@@ -1,19 +1,24 @@
 const ui = require("ui_lib/library");
 
 ui.addMenuButton("Reset UUID", "refresh", () => {
-    Vars.ui.showConfirm("Reset UUID", "Are you sure you want to reset UUID? We recommend to save it first", () => {
-        Core.settings.put("uuid","");
-	Vars.ui.showInfo("UUID has been reseted.");
+    Vars.ui.showConfirm("Reset UUID", "Are you sure you want to reset UUID? We recommend saving it first.", () => {
+        Core.settings.put("uuid", "");
+        Vars.ui.showInfo("UUID has been reset.");
     });
 });
 
 ui.addMenuButton("Get UUID", "paste", () => {
-	Vars.ui.showTextInput("Your UUID is below.","UUID:",Vars.platform.getUUID()) {
+    Vars.ui.showTextInput("Your UUID is below.", "UUID:", Vars.platform.getUUID(), () => {
     });
 });
 
 ui.addMenuButton("Set UUID", "wrench", () => {
-	Vars.ui.showTextInput("[red]Be accurate with changing UUID maybe you will can't join servers. If you have problems reset UUID.","UUID:",Vars.platform.getUUID(), res => {
-        Core.settings.put("uuid",res);
-    });
+    Vars.ui.showTextInput(
+        "[red]Be accurate with changing the UUID; you may not be able to join servers. If you have problems, reset the UUID.",
+        "UUID:",
+        Vars.platform.getUUID(),
+        (res) => {
+            Core.settings.put("uuid", res);
+        }
+    );
 });
